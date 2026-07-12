d7b8e228cd1687926b7bb0b10f91e821f5b6637a
Sean McBride <spmcbride1201@gmail.com>
2026-07-12 15:16:37 -0400

    Fix #236: Consolidate Sean/Beth/Joey rooms into SimpleRoom
    ## Summary
    Fixes #236.
    
    - Add parameterized `SimpleRoom` (wallColor prop) shared by Sean/Beth/Joey.
    - Routes and `ROOM_COMPONENTS` map unchanged; Yoonah/Lobby/ChangingRoom stay separate.
    
    ## Test plan
    - [x] `npm test`

--- browser/react/components/Beth.tsx
+++ browser/react/components/Beth.tsx
@@ -1,33 +1,10 @@
 import { Component } from 'react';
-import Teleporter from './Teleporter.tsx';
-import Room from './Room.tsx';
+import SimpleRoom from './SimpleRoom.tsx';
 
 // WebRTC chat join/leave is handled centrally by <App> on scene change (issue #70).
+// Layout lives in SimpleRoom; this file only pins the wall color (issue #236).
 export default class Beth extends Component {
   render() {
-    return (
-      <a-entity position="0 0 0">
-        <Room
-          floorWidth="50"
-          floorHeight="50"
-          wallHeight="25"
-          wallColor="orange"
-          floorColor=""
-          floorTexture="/img/carpet2.jpg"
-          ceilingColor="#998403"
-        />
-        <Teleporter
-          color="green"
-          label="Lobby"
-          href="/vr"
-          rotation="90"
-          x="-24.5"
-          y="1"
-          z="-5"
-          labelx="-1"
-          labely="1"
-        />
-      </a-entity>
-    );
+    return <SimpleRoom wallColor="orange" />;
   }
 }
